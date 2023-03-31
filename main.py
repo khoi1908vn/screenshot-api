6aaea141c14a2aebb4042cfa2cdf5420de989b34
khoi1908vn <khoimle7@gmail.com>
2023-04-01 00:30:38 +0700

    changed method

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-
-from pyppeteer import launch
-import asyncio,os, requests, subprocess
+import asyncio,os, requests
 from aiohttp import web, ClientSession
 
 
-async def get_screenshot(url, resolution: int = 720, delay: int = 0):
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+async def get_screenshot(url, resolution: int, delay: int = 7):
+    global ip
     window_height = int(resolution*16/9)
     window_width = resolution
-    browser = await launch(executablePath = subprocess.check_output(['which', 'chromium']).strip().decode('utf-8'), headless=True, args=['--no-sandbox', '--disable-dev-shm-usage', '--disable-gpu', '--window-position=0,0', '--enable-features=WebContentsForceDark'], handleSIGINT=False, handleSIGTERM=False, handleSIGHUP=False)
-    page = await browser.newPage()
-    await page.setViewport({'width': window_width, 'height': window_height})
-    await page._client.send('Page.setDownloadBehavior', {'behavior': 'allow', 'downloadPath': '/dev/null'})
-    await page.goto(url)
-    await page.waitForNavigation()
-    await asyncio.sleep(3 + delay)
-    elements = await page.Jx(f"//*[contains(text(), '{ip}')]")
-    for element in elements:
-        await page.evaluate("(element) => element.innerText = '<the host ip address>'", element)
-    image_bytes = await page.screenshot()
-    await browser.close()
-    return image_bytes
+    options = Options()
+    for arg in ['--no-sandbox', '--disable-dev-shm-usage', '--headless', '--disable-gpu', '--window-position=0,0', f'--window-size={window_height},{window_width}', '--enable-features=WebContentsForceDark']: options.add_argument(arg)
+    prefs = {
+    "download_restrictions": 3,
+    "download.open_pdf_in_system_reader": False,
+    "download.prompt_for_download": True,
+    "download.default_directory": "/dev/null",
+    "plugins.always_open_pdf_externally": False
+    }
+    options.add_experimental_option("prefs", prefs)
+    async with webdriver.Chrome(options=options) as driver:
+        await driver.get(url)
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.presence_of_element_located((By.XPATH, "//body[not(@class='loading')]")))
+        await asyncio.sleep(3 + delay)
+        elements = driver.find_elements(By.XPATH, f"//*[contains(text(), '{ip}')]")
+        for element in elements: driver.execute_script("arguments[0].innerText = arguments[1];", element, '<the host ip address>')
+        image_bytes = await driver.get_screenshot_as_png()
+    return await image_bytes
 
 routes = web.RouteTableDef()
 
