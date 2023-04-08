27c566fcec55cc464bafc6e9ef9dc9fcfb83116d
khoi1908vn <khoimle7@gmail.com>
2023-04-08 08:42:27 +0700

    typo

--- main.sh
+++ main.sh
@@ -1,19 +1,17 @@
 if [ "$NO_GIT_AUTOMATION" = "YES" ]; then
-    echo ========================================== Updating git ==========================================
+    echo "========================================== Updating git =========================================="
     git pull
     git reset --hard origin/main
 fi
 
-if [ "$NO_PIP_INSTALLED" -= "YES" ]; then
-    echo ========================================== Installing dependencies ==========================================
+if [ "$NO_PIP_INSTALLED" = "YES" ]; then
+    echo "========================================== Installing dependencies =========================================="
     apt-get update && apt-get install -y python3-pip
 fi
 
 if [ "$IS_REPLIT" != "YES" ]; then
-    python -m pip install -r requirements.txt
+    python3 -m pip install -r requirements.txt
 fi
 
-echo ========================================== Starting ==========================================
+echo "========================================== Starting =========================================="
 uvicorn main:app --reload --port 8080 --host 0.0.0.0
-
-
